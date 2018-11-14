a4ff128ccdeb034914a89208a94465f7df231f17
Severiano Badajoz <sevebadajoz@gmail.com>
2018-11-13 16:04:40 -0800

    Run through linter

--- js/ui.js
+++ js/ui.js
@@ -2,13 +2,13 @@ var OregonH = OregonH || {};
 
 OregonH.UI = {};
 
-//show a notification in the message area
-OregonH.UI.notify = function(message, type){
-  console.log(message + ' - ' + type);
+// show a notification in the message area
+OregonH.UI.notify = function notify(message, type) {
+  console.log(`${message} - ${type}`);
 };
 
 
-//refresh visual caravan stats
-OregonH.UI.refreshStats = function() {
+// refresh visual caravan stats
+OregonH.UI.refreshStats = function refreshStats() {
   console.log(this.caravan);
-}
+};
