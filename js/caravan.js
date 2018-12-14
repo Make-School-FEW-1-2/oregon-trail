138a934f87bfa876f283fe632ffab09c0f8f207a
Severiano Badajoz <sevebadajoz@gmail.com>
2018-12-13 21:53:04 -0800

    Fix caravan constants

--- js/caravan.js
+++ js/caravan.js
@@ -7,6 +7,22 @@ class Caravan {
     this.oxen = stats.oxen;
     this.money = stats.money;
     this.firepower = stats.firepower;
+
+    Caravan.constants = {
+      WEIGHT_PER_OX: 20,
+      WEIGHT_PER_PERSON: 2,
+      FOOD_WEIGHT: 0.6,
+      FIREPOWER_WEIGHT: 5,
+      GAME_SPEED: 800,
+      DAY_PER_STEP: 0.2,
+      FOOD_PER_PERSON: 0.02,
+      FULL_SPEED: 5,
+      SLOW_SPEED: 3,
+      FINAL_DISTANC: 1000,
+      EVENT_PROBABILITY: 0.15,
+      ENEMY_FIREPOWER_AVG: 5,
+      ENEMY_GOLD_AVG: 50,
+    };
   }
 
   // update weight and capacity
@@ -15,16 +31,18 @@ class Caravan {
     let droppedGuns = 0;
 
     // how much can the caravan carry
-    this.capacity = this.oxen * OregonH.WEIGHT_PER_OX + this.crew * OregonH.WEIGHT_PER_PERSON;
+    this.capacity = this.oxen * Caravan.constants.WEIGHT_PER_OX
+      + this.crew * Caravan.constants.WEIGHT_PER_PERSON;
 
     // how much weight do we currently have
-    this.weight = this.food * OregonH.FOOD_WEIGHT + this.firepower * OregonH.FIREPOWER_WEIGHT;
+    this.weight = this.food * Caravan.constants.FOOD_WEIGHT
+      + this.firepower * Caravan.constants.FIREPOWER_WEIGHT;
 
     // drop things behind if it's too much weight
     // assume guns get dropped before food
     while (this.firepower && this.capacity <= this.weight) {
       this.firepower -= 1;
-      this.weight -= OregonH.FIREPOWER_WEIGHT;
+      this.weight -= Caravan.constants.FIREPOWER_WEIGHT;
       droppedGuns += 1;
     }
 
@@ -34,7 +52,7 @@ class Caravan {
 
     while (this.food && this.capacity <= this.weight) {
       this.food -= 1;
-      this.weight -= OregonH.FOOD_WEIGHT;
+      this.weight -= Caravan.constants.FOOD_WEIGHT;
       droppedFood += 1;
     }
 
@@ -46,13 +64,14 @@ class Caravan {
   // update covered distance
   updateDistance() {
     const diff = this.capacity - this.weight;
-    const speed = OregonH.SLOW_SPEED + diff / this.capacity * OregonH.FULL_SPEED;
+    const speed = Caravan.constants.SLOW_SPEED
+      + diff / this.capacity * Caravan.constants.FULL_SPEED;
     this.distance += speed;
   }
 
   // food consumption
   consumeFood() {
-    this.food -= this.crew * OregonH.FOOD_PER_PERSON;
+    this.food -= this.crew * Caravan.constants.FOOD_PER_PERSON;
 
     if (this.food < 0) {
       this.food = 0;
